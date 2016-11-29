eb2228a4fca8b51327d579170eba01a9d10e9b07
magdamagda <magdamal@poczta.onet.eu>
2016-11-29 16:21:36 +0100

    genetic algorithm - general approach

--- geneticalgorithm.cpp
+++ geneticalgorithm.cpp
@@ -2,5 +2,88 @@
 
 GeneticAlgorithm::GeneticAlgorithm()
 {
+    this->populationSize = 50;
+    this->k = 10;
+    this->crossOverProbability = 0.8;
+    this->mutationProbability = 0.2;
+    this->iterations = 20;
+}
 
+void GeneticAlgorithm::setParameters(int size, int k, double crossOverProb, double mutationProb, int iter){
+    this->populationSize = size;
+    this->k = k;
+    this->crossOverProbability = crossOverProb;
+    this->mutationProbability = mutationProb;
+    this->iterations = iter;
+}
+
+Solution* GeneticAlgorithm::run(){
+    int i = 0;
+    Solution* best = nullptr;
+    initialize();
+    evaluate();
+    while(i<this->iterations){
+        select();
+        crossOver();
+        mutate();
+        replace();
+        evaluate();
+        best = findBest();
+        cout<<"Iteration "<<i<<" , best instance crossings: "<<best->crossings<<endl;
+        i++;
+    }
+    return best;
+}
+
+void GeneticAlgorithm::initialize(){
+
+}
+
+void GeneticAlgorithm::select(){
+
+}
+
+void GeneticAlgorithm::evaluate(){
+
+}
+
+void GeneticAlgorithm::crossOver(){
+
+}
+
+void GeneticAlgorithm::crossOverVertices(){
+
+}
+
+void GeneticAlgorithm::crossOverPages(){
+
+}
+
+void GeneticAlgorithm::mutate(){
+
+}
+
+void GeneticAlgorithm::mutateVertices(){
+
+}
+
+void GeneticAlgorithm::mutatePages(){
+
+}
+
+void GeneticAlgorithm::replace(){
+
+}
+
+Solution* GeneticAlgorithm::findBest(){
+    if (this->population.size() == 0){
+        return nullptr;
+    }
+    Solution* best = this->population[0];
+    for(auto it=this->population.begin()+1; it!=this->population.end(); ++it){
+        if(best->crossings>(*it)->crossings){
+            best = *it;
+        }
+    }
+    return best;
 }
--- geneticalgorithm.h
+++ geneticalgorithm.h
@@ -1,11 +1,45 @@
 #ifndef GENETICALGORITHM_H
 #define GENETICALGORITHM_H
 
+#include "solution.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 class GeneticAlgorithm
 {
 public:
     GeneticAlgorithm();
+    void setParameters(int size, int k, double crossOverProb, double mutationProb, int iter);
+    Solution* run();
+
+private:
+    void initialize();
+    void select();
+    void evaluate();
+    void crossOver();
+    void crossOverVertices();
+    void crossOverPages();
+    void mutate();
+    void mutateVertices();
+    void mutatePages();
+    void replace();
+    Solution* findBest();
+
+    double crossOverProbability;
+    double mutationProbability;
+    int k; // how many the best parents is inserted to the new population
+    int populationSize;
+    int iterations;
+
+    vector<Solution*> population;
+    vector<Solution*> newPopulation;
+
+    static bool compare_function (Solution* i, Solution j){
+        return (i->crossings>j.crossings);
+    }
+
 };
 
 #endif // GENETICALGORITHM_H
